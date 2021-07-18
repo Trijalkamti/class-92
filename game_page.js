c10150ddbc5d9c954ef1f5ca337d3202abe890a6
Trijalkamti <82204283+Trijalkamti@users.noreply.github.com>
2021-07-18 12:52:46 +0530

    Add files via upload

---
+++ game_page.js
@@ -0,0 +1,87 @@
+player1_name = localStorage.getItem("player1_name");
+player2_name = localStorage.getItem("player2_name");
+
+player1_score = 0;
+player2_score = 0;
+
+document.getElementById("player1_name").innerHTML = player1_name + " : ";
+document.getElementById("player2_name").innerHTML = player2_name + " : ";
+
+document.getElementById("player1_score").innerHTML = player1_score ;
+document.getElementById("player2_score").innerHTML = player2_score ;
+
+document.getElementById("player_question").innerHTML = "Question Turn - " + player1_name ;
+document.getElementById("player_answer").innerHTML = "Answer Turn - " + player2_name ;
+
+
+function send() {
+    get_word = document.getElementById("word").value;
+    word = get_word.toLowerCase();
+    console.log("word in lowerCase = " + word);
+
+    charAt1 = word.chartAt(1);
+    console.log(charAt1);
+
+    length_divide_2 = Math.floor(word.lenght/2);
+    chartAt2 = word.chartAt(length_divide_2);
+    console.log(chartAt2);
+
+    lenght_minus_1 = word.lenght - 1;
+    chartAt3 = word.chartAt(lenght_minus_1);
+    console.log(chartAt3);
+
+    remove_chartAt1 = word.replace(chartAt1, "_");
+    remove_chartAt2 = chartAt1.replace(chartAt2, "_");
+    remove_chartAt3 = chartAt2.replace(chartAt3, "_");
+    console.log(chartAt3);
+
+    question_word = "<h4 id='word_display'> Q. "+remove_chartAt3+"</h4>";
+    input_box = "<br>Answer : <input type='text' id='input_check_box'>";
+    check_button = "<br><br><button class='btn btn-info' onclick='check()'>Check</button>";
+    row = question_word + input_box + check_button ;
+    document.getElementById("output").innerHTML = row;
+document.getElementById("word").value = "";
+}
+
+function check()
+{
+    get_answer = document.getElementById("input_check_box").value;
+    answer = get_answer.toLowerCase();
+    console.log("answer in lower case - " + answer);
+    if(answer == word)
+    {
+        if(answer_turn == "player1")
+        {
+            player1_score = player1_score +1;
+            document.getElementById("player1_score").innerHTML = player1_score;
+        }
+        else
+        {
+            player2_score = player2_score +1;
+            document.getElementById("player2_score").innerHTML = player2_score;
+
+        }
+    }
+    if(question_turn == "player1")
+    {
+        question_turn == "player2"
+        document.getElementById("player_question").innerHTML = "Question turn - " + player2_name;
+    }
+    else
+    {
+        question_turn = "player1"
+        document.getElementById("player_question").innerHTML = "Question turn - " + player1_name;
+    }
+    if(answer_turn == "player1")
+    {
+        answer_turn = "player2"
+        document.getElementById("player_answer").innerHTML = "Answer turn - " + player2_name;
+    }
+    else
+    {
+        answer_turn = "player1"
+        document.getElementById("player_answer").innerHTML = "Answer turn - " + player1_name;
+    }
+
+    document.getElementById("output").innerHTML = "";
+}
